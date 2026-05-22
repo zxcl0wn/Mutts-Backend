d6c4d0fa1e9cb47bbc3c77071dbd8a2e0e176020
Gleb <m_marple1@mail.ru>
2026-05-22 19:37:07 +1000

    feat: add new unit types

--- backend/app/enums/unit_type.py
+++ backend/app/enums/unit_type.py
@@ -1,7 +1,19 @@
 from enum import Enum
 
+
 class UnitType(str, Enum):
     """Типы юнитов"""
-    WARRIOR = "warrior"
-    ARCHER = "archer"
-    MAGE = "mage"
+
+    ARCHITECT = "ARCHITECT"
+    BARRICADE = "BARRICADE"
+    BERSERKER = "BERSERKER"
+    BULLDOZER = "BULLDOZER"
+    CATAPULT = "CATAPULT"
+    DRONE = "DRONE"
+    GUNNER = "GUNNER"
+    MEDIC = "MEDIC"
+    PHANTOM = "PHANTOM"
+    SCRAMBLER = "SCRAMBLER"
+    SNIPER = "SNIPER"
+    TECHNICIAN = "TECHNICIAN"
+    TURRET = "TURRET"
